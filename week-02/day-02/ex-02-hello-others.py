e5ce4e9bfa57d86c717c891dee5ce0dee1de93e3
Megyesi Dora <dora.megyesi@gmail.com>
2017-03-21 11:24:08 +0100

    Types are done, ex 01-03

---
+++ week-02/day-02/ex-02-hello-others.py
@@ -0,0 +1,10 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a = "Hello,"
+>>> print(a + "Balazs!")
+Hello,Balazs!
+>>> print(a + "Pati!")
+Hello,Pati!
+>>> print(a + "Bajusz!")
+Hello,Bajusz!
+>>> 
