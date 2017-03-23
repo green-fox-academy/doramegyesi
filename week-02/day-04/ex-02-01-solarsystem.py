ca65c9e8bd59cfd5a4ba4f88d39aff78f2eaa130
Megyesi Dora <dora.megyesi@gmail.com>
2017-03-23 15:57:01 +0100

    alternative way for inserting, I am back on track!

--- week-02/day-04/ex-02-01-solarsystem.py
+++ week-02/day-04/ex-02-01-solarsystem.py
@@ -4,3 +4,10 @@
 planetList = ["Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"]
 planetList.insert(5, "Saturn")
 print(planetList)
+
+#or:
+
+planetList = ["Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"]
+pl = ["Saturn"]
+planetList = [planetList[:5] + pl + planetList[5:]]
+print(planetList)
