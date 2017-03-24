cc1dec5192abc90785d3b7bb9f2cfba9e0b6f7e8
Megyesi Dora <dora.megyesi@gmail.com>
2017-03-24 11:20:58 +0100

    made the game work

--- week-02/day-05/anagram.py
+++ week-02/day-05/anagram.py
@@ -1,7 +1,16 @@
 
-a = "ipgeno"
-b = "pigeon"
+a = input("I want to play a game. Gimme a word! - ")
+b = input("I need another one to play with! I will tell you if they are anagrams. - ")
 
 def anagram(a, b):
-    if len(a) != len (b):
-        print(False)
+    for l in b:
+        if l not in a:
+            return False
+        else:
+            return True
+    for l in a:
+        if l not in b:
+            return False
+        else:
+            return True
+print(anagram(a, b))
