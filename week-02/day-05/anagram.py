365f56757bb0b62b72c7d6856e37d8fe0347d374
Megyesi Dora <dora.megyesi@gmail.com>
2017-03-24 11:36:46 +0100

    fixed it, now it compares the length again

--- week-02/day-05/anagram.py
+++ week-02/day-05/anagram.py
@@ -3,14 +3,17 @@ a = input("I want to play a game. Gimme a word! - ")
 b = input("I need another one to play with! I will tell you if they are anagrams. - ")
 
 def anagram(a, b):
-    for l in b:
-        if l not in a:
-            return False
-        else:
-            return True
-    for l in a:
-        if l not in b:
-            return False
-        else:
-            return True
+    if len(a) == len(b):
+        for l in b:
+            if l not in a:
+                return False
+            else:
+                return True
+        for l in a:
+            if l not in b:
+                return False
+            else:
+                return True
+    else:
+        return False
 print(anagram(a, b))
