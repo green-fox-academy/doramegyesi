5732fda7153632556384de4b33570f2a55ba3603
Megyesi Dora <dora.megyesi@gmail.com>
2017-03-28 16:17:43 +0200

    now returns zero if file does not exist

--- week-03/day-02/file_not_exists.py
+++ week-03/day-02/file_not_exists.py
@@ -1,13 +1,17 @@
 # write a function that takes a filename and returns the number of lines the
 # file contains. It should return zero if the file does not exist.
 
-text = open("reversed_lines.txt", "r")
-t = text.readlines()
-text.close()
 
-def total_lines(t):
-    for lines in range(len(t)):
-        lines += 1
-    print(lines)
 
-total_lines(t)
+def total_lines(kutya):
+    try:
+        text = open(kutya, "r")
+        t = text.readlines()
+        text.close()
+        for lines in range(len(t)):
+            lines += 1
+        print(lines)
+    except FileNotFoundError:
+        print("0")
+
+total_lines("reversed_lines.txt")
