270a9623c29477a78e5b94ea1afa43196cd8bb2f
Megyesi Dora <dora.megyesi@gmail.com>
2017-03-28 17:03:29 +0200

    works way better with a loop

--- week-03/day-02/crypto_3revorder.py
+++ week-03/day-02/crypto_3revorder.py
@@ -1,7 +1,21 @@
+# Create a method that decrypts texts/reversed_order.txt
+
 def decrypt(stuff):
     text = open(stuff, "r")
     stuff = text.readlines()
     text.close()
     print(stuff[::-1])
+
+decrypt("reversed_order.txt")
+
+
+def decrypt(thing):
+    text = open(thing, "r")
+    thing = text.readlines()
+    text.close()
+    new = ""
+    for b in thing[::-1]:
+        new += b
+    print(new)
 
 decrypt("reversed_order.txt")
