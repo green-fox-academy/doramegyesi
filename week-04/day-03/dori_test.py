d14290a75d8c496fa885d9360cc36f2f1a7ca8d6
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-05 14:31:53 +0200

    sum works, first test is done

--- week-04/day-03/dori_test.py
+++ week-04/day-03/dori_test.py
@@ -1,11 +1,18 @@
 import unittest
-from dori_work import Apples
+from dori_work import *
 
 class TestApples(unittest.TestCase):
     def test_get_apple(self):
         my_object = Apples()
         my_object.get_apple()
         self.assertEquals(my_object.get_apple(), "apple")
+
+
+class TestSum_stuff(unittest.TestCase):
+    def test_sum_all(self):
+        new_individual = Sum_stuff([6, 7])
+        self.assertEquals(new_individual.sum_all(), 13)
+
 
 if __name__ == '__main__':
     unittest.main()
--- week-04/day-03/dori_work.py
+++ week-04/day-03/dori_work.py
@@ -4,3 +4,13 @@ class Apples:
 
     def get_apple(self):
         return self.apple
+
+class Sum_stuff:
+    def __init__(self, list_of_numbers = []):
+        self.list_of_numbers = list_of_numbers
+
+    def sum_all(self):
+        numbers = 0
+        for n in self.list_of_numbers:
+            numbers += n
+        return numbers
