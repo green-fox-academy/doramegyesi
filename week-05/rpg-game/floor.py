fe53c027df295d5d924db013cb81ff8d8e3346e2
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-12 10:04:03 +0200

    pictures are added

--- week-05/rpg-game/floor.py
+++ week-05/rpg-game/floor.py
@@ -22,6 +22,12 @@ class Tile():
     def __init__(self):
         self.img_floor = PhotoImage(file = "floor.png")
         self.img_wall = PhotoImage(file = "wall.png")
+        self.img_hero_up = PhotoImage(file = "hero-up.png")
+        self.img_hero_down = PhotoImage(file = "hero-down.png")
+        self.img_hero_left = PhotoImage(file = "hero-left.png")
+        self.img_hero_right = PhotoImage(file = "hero-right.png")
+        self.img_boss = PhotoImage(file = "boss.png")
+        self.img_skeleton = PhotoImage(file = "skeleton.png")
 
     def draw(self):
         first_square = canvas.create_image(36, 36, image = self.img_floor)
