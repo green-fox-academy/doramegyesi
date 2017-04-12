9339631e61db752aaf886485c64b23465a634671
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-12 11:34:49 +0200

    new FieldMap class, code is on its way to be refactored

--- week-05/rpg-game/floor.py
+++ week-05/rpg-game/floor.py
@@ -6,22 +6,27 @@ canvas = Canvas(root, width = 720, height = 720, background = "black")
 canvas.pack()
 
 map = [
-	[0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0]
+	[0,0,1,0,0,,0,0,0,0],
+	[0,1,1,0,0,1,0,0,0,0],
+	[0,1,0,0,0,1,1,1,0,0],
+	[0,0,0,0,0,0,1,0,0,0],
+	[0,1,1,0,0,0,1,0,0,0],
+	[0,0,1,0,0,0,0,0,0,1],
+	[0,0,0,0,0,0,0,0,1,0],
+    [0,0,0,0,1,0,0,0,1,0],
+    [0,1,0,0,1,1,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0]
 ]
 
-class Tile():
-    def __init__(self):
+class FieldMap():
+    def __init___(self):
         self.img_floor = PhotoImage(file = "floor.png")
         self.img_wall = PhotoImage(file = "wall.png")
+
+class Characters():
+    def __init__(self):
+        #self.img_floor = PhotoImage(file = "floor.png")
+        #self.img_wall = PhotoImage(file = "wall.png")
         self.img_hero_up = PhotoImage(file = "hero-up.png")
         self.img_hero_down = PhotoImage(file = "hero-down.png")
         self.img_hero_left = PhotoImage(file = "hero-left.png")
@@ -29,10 +34,6 @@ class Tile():
         self.img_boss = PhotoImage(file = "boss.png")
         self.img_skeleton = PhotoImage(file = "skeleton.png")
 
-    def draw(self):
-        first_square = canvas.create_image(36, 36, image = self.img_floor)
 
-floor = Tile()
-floor.draw()
 
 root.mainloop()
