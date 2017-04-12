a4900fcd95f9ee5afd9075f27a48ed7c44193e0e
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-12 15:27:29 +0200

    the hero is drawn

--- week-05/rpg-game/floor.py
+++ week-05/rpg-game/floor.py
@@ -6,13 +6,13 @@ canvas = Canvas(root, width = 720, height = 720, background = "black")
 canvas.pack()
 
 map_1 = [
-	[0,0,1,0,0,1,0,0,0,0],
+	[0,0,1,0,0,1,0,0,1,0],
 	[0,1,1,0,1,1,0,0,0,0],
 	[0,1,0,0,0,1,1,1,0,0],
 	[0,0,0,0,0,0,1,0,0,0],
-	[0,1,1,0,0,0,1,0,0,0],
+	[1,1,1,0,0,0,1,0,0,0],
 	[0,0,1,0,0,0,0,0,0,1],
-	[0,0,1,0,1,0,0,0,1,0],
+	[0,0,1,0,1,0,1,1,1,0],
     [0,0,0,0,1,0,0,1,1,0],
     [0,1,1,0,1,1,0,0,0,0],
     [0,0,0,0,0,1,0,0,0,0]
@@ -34,16 +34,21 @@ class FieldMap():
 
 class Characters():
     def __init__(self):
-        #self.img_floor = PhotoImage(file = "floor.png")
-        #self.img_wall = PhotoImage(file = "wall.png")
         self.img_hero_up = PhotoImage(file = "hero-up.png")
         self.img_hero_down = PhotoImage(file = "hero-down.png")
         self.img_hero_left = PhotoImage(file = "hero-left.png")
         self.img_hero_right = PhotoImage(file = "hero-right.png")
         self.img_boss = PhotoImage(file = "boss.png")
         self.img_skeleton = PhotoImage(file = "skeleton.png")
+        self.position_x = 0
+        self.position_y = 0
+
+    def draw_hero(self, x, y):
+        canvas.create_image(x, y, anchor = NW, image = self.img_hero_down)
 
 field = FieldMap()
 field.draw_map()
+hero = Characters()
+hero.draw_hero(0,0)
 
 root.mainloop()
