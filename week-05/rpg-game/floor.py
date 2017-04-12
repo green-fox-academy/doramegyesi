350893ddba2d1826e508f09529582c026ca501e5
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-12 14:05:26 +0200

    draw_map method works

--- week-05/rpg-game/floor.py
+++ week-05/rpg-game/floor.py
@@ -5,32 +5,32 @@ root = Tk()
 canvas = Canvas(root, width = 720, height = 720, background = "black")
 canvas.pack()
 
-map = [
+map_1 = [
 	[0,0,1,0,0,1,0,0,0,0],
-	[0,1,1,0,0,1,0,0,0,0],
+	[0,1,1,0,1,1,0,0,0,0],
 	[0,1,0,0,0,1,1,1,0,0],
 	[0,0,0,0,0,0,1,0,0,0],
 	[0,1,1,0,0,0,1,0,0,0],
 	[0,0,1,0,0,0,0,0,0,1],
-	[0,0,0,0,0,0,0,0,1,0],
-    [0,0,0,0,1,0,0,0,1,0],
-    [0,1,0,0,1,1,0,0,0,0],
+	[0,0,1,0,1,0,0,0,1,0],
+    [0,0,0,0,1,0,0,1,1,0],
+    [0,1,1,0,1,1,0,0,0,0],
     [0,0,0,0,0,1,0,0,0,0]
 ]
 
 class FieldMap():
-    def __init___(self):
-        #self.img_floor = PhotoImage(file = "floor.png")
-        #self.img_wall = PhotoImage(file = "wall.png")
+    def __init__(self):
+        self.img_floor = PhotoImage(file = "floor.png")
+        self.img_wall = PhotoImage(file = "wall.png")
 
-        def draw_map(self, map):
-            tile = 72
-            for row in range(len(map)):
-                for column in range (len(map[row])):
-                    if map[row][column] == 0:
-                        canvas.create_image(row*tile, column*tile, anchor = nw, image = PhotoImage(file = "floor.png")
-                    else:
-                        canvas.create_image(row*tile, column*tile, anchor = nw, image = PhotoImage(file = "wall.png")
+    def draw_map(self):
+        tile = 72
+        for row in range(len(map_1)):
+            for column in range (len(map_1[row])):
+                if map_1[row][column] == 0:
+                    canvas.create_image(row*tile, column*tile, anchor = NW, image = self.img_floor)
+                else:
+                    canvas.create_image(row*tile, column*tile, anchor = NW, image = self.img_wall)
 
 class Characters():
     def __init__(self):
@@ -43,7 +43,7 @@ class Characters():
         self.img_boss = PhotoImage(file = "boss.png")
         self.img_skeleton = PhotoImage(file = "skeleton.png")
 
-field = FieldMap
-field.draw_map(map)
+field = FieldMap()
+field.draw_map()
 
 root.mainloop()
