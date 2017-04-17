284c94c31ca46a6ee0974a83ca04233c5f65da66
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-17 09:59:33 +0200

    subclasses are more detailed now

--- week-04/day-02/garden_application.py
+++ week-04/day-02/garden_application.py
@@ -6,11 +6,22 @@ class Garden:
     def watering(self, watering):
         pass
 
+    def print_status(self):
+        pass
+
 class Flower(Garden):
-    def __init__(self, color = "black", water_level = 0):
+    def __init__(self, color, water_level = 0):
         self.color = color
         self.water_level = water_level
+        self.flowers.append([self.color, self.water_level])
 
 class Tree(Garden):
-    def __init__(self, color = "black"):
+    def __init__(self, color, water_level = 0):
         self.color = color
+        self.water_level = water_level
+        self.trees.append([self.color, self.water_level])
+
+flower_one =  Flower("yellow")
+flower_two = Flower("blue")
+tree_one = Tree("purple")
+tree_two = Tree("orange")
