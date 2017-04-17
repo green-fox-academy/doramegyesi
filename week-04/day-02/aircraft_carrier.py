704e39d9f0731c8a8aa4ebfdee24d2ada433c905
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-17 15:40:12 +0200

    refill method partly works

--- week-04/day-02/aircraft_carrier.py
+++ week-04/day-02/aircraft_carrier.py
@@ -16,7 +16,6 @@ class Aircraft():
         if given_ammo <= self.max_ammo:
             self.ammo += given_ammo
             given_ammo -= self.ammo
-            return given_ammo
         elif given_ammo > self.max_ammo:
             self.ammo == self.max_ammo
             given_ammo -= self.max_ammo
@@ -26,13 +25,19 @@ class Aircraft():
         return self.type_of_aircraft
 
     def get_status(self):
-        status = "type:", self.type_of_aircraft, "ammo:", self.ammo, "base_damage:", self.base_damage, "all damage:" #all damage here
+        damage = self.fight()
+        status = "type:", str(self.type_of_aircraft), "ammo:", str(self.ammo), "base_damage:", str(self.base_damage), "all damage:", str(damage)
         return status
 
-class F16(Aircraft):
-    def __init__(self):
-        pass
+#class F16(Aircraft):
+#    def __init__(self, type_of_aircraft, ammo = 0, max_ammo = 8, base_damage =30):
+#        pass
 
-class F35(Aircraft):
-    def __init__(self):
-        pass
+#class F35(Aircraft):
+#    def __init__(self, type_of_aircraft, ammo = 0, max_ammo = 12, base_damage = 50):
+#        pass
+
+aircraft_one = Aircraft("F16", 0, 8, 30)
+aircraft_two = Aircraft("F35", 0, 12, 50)
+print(aircraft_one.refill(100))
+print(aircraft_one.get_status())
