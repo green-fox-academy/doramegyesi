a91afa53336ef72dcfbc33cfd3444621c6e1c0cb
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-17 11:05:27 +0200

    watering method works but not properly

--- week-04/day-02/garden_application.py
+++ week-04/day-02/garden_application.py
@@ -14,14 +14,14 @@ class Garden():
             else:
                 print("the", n[0], "tree does not need water")
 
-    def watering_plants(self, water_amount = 0):
+    def watering_plants(self, water_amount):
         self.water_amount = water_amount
         for n in self.flowers:
             if n[1] < 5:
-                n[1] == n[1] + water_amount * 0.75
+                n[1] += water_amount * 0.75
         for n in self.trees:
             if n[1] < 10:
-                n[1] == n[1] + water_amount * 0.4
+                n[1] += water_amount * 0.4
 
 class Flower(Garden):
     def __init__(self, color, water_level = 0):
@@ -40,5 +40,5 @@ flower_two = Flower("blue")
 tree_one = Tree("purple")
 tree_two = Tree("orange")
 Garden.print_status(Garden)
-Garden.watering_plants(40)
+Garden.watering_plants(Garden, 40)
 Garden.print_status(Garden)
