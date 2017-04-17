a557f38009351ecdd4290014b31c0089819c1514
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-17 14:30:45 +0200

    constructor of Aircraft class and the fight method are more detailed now

--- week-04/day-02/aircraft_carrier.py
+++ week-04/day-02/aircraft_carrier.py
@@ -1,14 +1,21 @@
 class Aircraft():
-    def __init__(self):
+    def __init__(self, type_of_aircraft, ammo = 0, max_ammo = 0, base_damage = 0):
+        self.type_of_aircraft = type_of_aircraft
+        self.ammo = ammo
+        self.max_ammo = max_ammo
+        self.base_damage = base_damage
         pass
 
     def fight(self):
+        damage = self.ammo * self.base_damage
+        ammo = 0
+        return damage
         #resets ammo to 0
         #returns damage
         #damage = base damage * ammo
-        pass
 
-    def refill(self):
+    def refill(self, given_ammo):
+        self.given_ammo = given_ammo
         #from the given number it takes as much ammo as it can
         #max the max ammo
         #returns ramaining ammo
