9d2813511ebba181cc80b58a1a3d7d7c35d56c0a
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-17 14:14:47 +0200

    basic plan for the first part of the exercise

---
+++ week-04/day-02/aircraft_carrier.py
@@ -0,0 +1,31 @@
+class Aircraft():
+    def __init__(self):
+        pass
+
+    def fight(self):
+        #resets ammo to 0
+        #returns damage
+        #damage = base damage * ammo
+        pass
+
+    def refill(self):
+        #from the given number it takes as much ammo as it can
+        #max the max ammo
+        #returns ramaining ammo
+        pass
+
+    def get_type(self):
+        #returns type as string
+        pass
+
+    def get_status(self):
+        #returns type, ammo, base damage and all damage as string
+        pass
+
+class F16(Aircraft):
+    def __init__(self):
+        pass
+
+class F35(Aircraft):
+    def __init__(self):
+        pass
