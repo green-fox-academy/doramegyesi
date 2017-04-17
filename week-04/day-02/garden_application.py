0628f2d00803ef37bc79107dad589b4409336b96
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-17 10:25:33 +0200

    prints status quite well

--- week-04/day-02/garden_application.py
+++ week-04/day-02/garden_application.py
@@ -1,13 +1,18 @@
-class Garden:
-    def __init__(self):
-        self.flowers = []
-        self.trees = []
-
-    def watering(self, watering):
-        pass
+class Garden():
+    flowers = []
+    trees = []
 
     def print_status(self):
-        pass
+        for n in self.flowers:
+            if n[1] < 5:
+                print("the", n[0], "flower needs water")
+            else:
+                print("the", n[0], "flower does not need water")
+        for n in self.trees:
+            if n[1] < 10:
+                print("the", n[0], "tree needs water")
+            else:
+                print("the", n[0], "tree does not need water")
 
 class Flower(Garden):
     def __init__(self, color, water_level = 0):
@@ -22,6 +27,7 @@ class Tree(Garden):
         self.trees.append([self.color, self.water_level])
 
 flower_one =  Flower("yellow")
-flower_two = Flower("blue")
+flower_two = Flower("blue", 10)
 tree_one = Tree("purple")
-tree_two = Tree("orange")
+tree_two = Tree("orange", 15)
+Garden.print_status(Garden)
