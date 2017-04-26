4a0d7f21ebab572a393e5ccb44501b60bfc299ce
Megyesi Dora <dora.megyesi@gmail.com>
2017-04-26 07:15:42 +0200

    added ex-14, works

---
+++ week-02/day-03/ex-14-double-items.py
@@ -0,0 +1,9 @@
+# - Create an array variable named `ag`
+#   with the following content: `[3, 4, 5, 6, 7]`
+# - Double all the values in the array
+
+ag = [3, 4, 5, 6, 7]
+for a in range(0, 5):
+    ag[a] *= 2
+
+print(ag)
