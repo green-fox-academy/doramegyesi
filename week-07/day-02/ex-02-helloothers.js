c7c9c3b91facff9c9194543c881ea52185e37cc0
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-02 13:39:07 +0200

    works

---
+++ week-07/day-02/ex-02-helloothers.js
@@ -0,0 +1,13 @@
+
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+console.log('Hello, Ron Swanson!')
+console.log('Hello, Maurice Moss!')
+console.log('Hello, Dwight Schrute!')
