2f5a9681320b0baf5d0033a6d48043508c28769a
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-02 15:09:27 +0200

    added the missing semicolons

--- week-07/day-02/ex-06-codinghours.js
+++ week-07/day-02/ex-06-codinghours.js
@@ -9,16 +9,16 @@
 // Print the percentage of the coding hours in the semester if the average
 // work hours weekly is 52
 
-var semester = 17
-var workdaysPerWeek = 5
-var dailyCodingHours = 6
-var averageWorkingHours = 52
-var totalCodingHours = semester * workdaysPerWeek * dailyCodingHours
+var semester = 17;
+var workdaysPerWeek = 5;
+var dailyCodingHours = 6;
+var averageWorkingHours = 52;
+var totalCodingHours = semester * workdaysPerWeek * dailyCodingHours;
 
-console.log('Hours spent with coding: ', totalCodingHours)
-console.log('Average: ', (dailyCodingHours * workdaysPerWeek / 52) * 100, '%')
+console.log('Hours spent with coding: ', totalCodingHours);
+console.log('Average: ', (dailyCodingHours * workdaysPerWeek / 52) * 100, '%');
 
 // without variables
 
-console.log('Hours spent with coding: ', 17 * 5 * 6)
-console.log('Average: ', (6 * 5 / 52) * 100, '%')
+console.log('Hours spent with coding: ', 17 * 5 * 6);
+console.log('Average: ', (6 * 5 / 52) * 100, '%');
