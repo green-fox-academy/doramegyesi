b40542bd5d01d8cda1dad6dcde3f62be0b4d0ba9
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-02 15:27:02 +0200

    works as required

---
+++ week-07/day-02/ex-08-basicinfo.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+var myName = 'Dori';
+var myAge = 31;
+var myHeight = 1.62;
+var myStatus = false;
+
+console.log(myName);
+console.log(myAge);
+console.log(myHeight);
+console.log(myStatus);
