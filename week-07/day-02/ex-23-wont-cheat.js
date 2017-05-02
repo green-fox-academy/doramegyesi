8f87216764236c9002ace9cfbbeaeed0b72ba509
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-02 23:05:22 +0200

    works well with while loop

---
+++ week-07/day-02/ex-23-wont-cheat.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+var noCheating = 0;
+while (noCheating < 100) {
+    console.log("I won't cheat on the exam!");
+    noCheating++;
+}
