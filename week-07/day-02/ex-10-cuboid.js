bc9418f8f215f7a6bd7663ec4e2f4e7aad44f6ff
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-02 16:25:00 +0200

    works as required

---
+++ week-07/day-02/ex-10-cuboid.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+
+var cuboidLength = 11
+var cuboidWidth = 8
+var cuboidHeight = 17
+
+console.log('Surface Area: ', 2 * (cuboidLength * cuboidWidth + cuboidLength * cuboidHeight + cuboidWidth * cuboidHeight))
+console.log('Volume: ', cuboidWidth * cuboidLength * cuboidHeight)
