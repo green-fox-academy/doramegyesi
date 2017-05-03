31e810fcc01956ce3524cdef9899ae926a1deafc
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-03 18:28:21 +0200

    how beautifully messed up this is

---
+++ week-07/day-03/14-append-a.js
@@ -0,0 +1,12 @@
+'use strict';
+// - Create a variable named `nimals`
+//   with the following content: `["kuty", "macs", "cic"]`
+// - Add all elements an `"a"` at the end
+// - try to use built in functions instead of loops
+
+var nimals = ['kuty', 'macsk', 'cic'];
+
+var animals = nimals.map(function(element) {
+    return nimals[element] += 'a';
+})
+console.log(nimals);
