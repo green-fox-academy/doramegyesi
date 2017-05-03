79916b47f6a25582f75f94292998ca565a4c3b08
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-03 18:09:53 +0200

    everything is undefined

---
+++ week-07/day-03/13-increment-element.js
@@ -0,0 +1,15 @@
+'use strict';
+// - Create a variable named `t` with the following content: `[1, 2, 3, 4, 5]`
+// - Increment the third element simply by accessing it
+// - Log the third element to the console
+
+var t = [1, 2, 3, 4, 5];
+
+var incremented = t.map(function(n) {
+    for (n; n < t.length; n++) {
+        return incremented;
+    }
+})
+
+console.log(incremented);
+console.log(incremented[2])
