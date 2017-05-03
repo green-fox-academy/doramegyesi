190c5cf042892c62e2c7a8425c10a448f7db1479
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-03 11:07:09 +0200

    corrected

--- week-07/day-02/ex-24-printeven.js
+++ week-07/day-02/ex-24-printeven.js
@@ -2,7 +2,6 @@
 
 // Create a program that prints all the even numbers between 0 and 500
 
-var num = 2;
-for (num = 2; num <= 500; num += 2) {
+for (var num = 2; num <= 500; num += 2) {
     console.log(num);
 }
