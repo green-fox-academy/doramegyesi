05e506eada0d173d7bab55eb66cb19a88b81afbc
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-03 21:04:13 +0200

    IT WORKS, and how beautiful it is!

---
+++ week-07/day-03/strings02-reverse.js
@@ -0,0 +1,10 @@
+'use strict';
+// Reverse the string!
+
+var reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+function reverse(string) {
+    return string.split("").reverse().join("");
+}
+
+console.log(reverse(reversed));
