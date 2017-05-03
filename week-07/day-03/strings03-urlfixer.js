496331814b350c9d1901d5b9ed22af1be0a6b29d
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-03 21:19:15 +0200

    string again!

--- week-07/day-03/strings03-urlfixer.js
+++ week-07/day-03/strings03-urlfixer.js
@@ -2,9 +2,9 @@
 // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
 // Also, the URL is missing a crutial component, find out what it is and insert it too!
 
-var url = "https//www.reddit.com/r/nevertellmethebots";
+var url = 'https//www.reddit.com/r/nevertellmethebots';
 
 var missingThingy = ':'
 
-url = [url.slice(0, 4), missingThingy, url.slice(5)];
+url = [url.slice(0, 4), missingThingy, url.slice(5)].join('');
 console.log(url);
