1fe3dfe61b7289ed859effe3e538059933968aa8
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-04 17:41:17 +0200

    works so nicely with for loop

--- week-07/day-04/05-content04.js
+++ week-07/day-04/05-content04.js
@@ -1,2 +1,9 @@
 /* replace the list items' content with items from this list:
    ['apple', 'banana', 'cat', 'dog'] */
+
+var list = document.querySelectorAll('li');
+var newList = ['apple', 'banana', 'cat', 'dog'];
+
+for (var e = 0; e < list.length; e++) {
+    list[e].textContent = newList[e];
+};
