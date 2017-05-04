29091ea0038acddb076ee265a999c68bc22ea8f7
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-04 17:47:06 +0200

    created the files

---
+++ week-07/day-04/06-attributes.js
@@ -0,0 +1,5 @@
+Write the image's url to the console.
+Replace the image with a picture of yourself.
+Make the link point to the Green Fox Academy website.
+Disable the second button.
+Replace its text with 'Don't click me!'.
