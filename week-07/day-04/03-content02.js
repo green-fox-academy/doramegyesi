f9d7c73650b5bbd32b5bd6078ea89e4d997569e8
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-04 16:35:59 +0200

    it works and how beautiful it is

--- week-07/day-04/03-content02.js
+++ week-07/day-04/03-content02.js
@@ -1,1 +1,6 @@
 //  fill every paragraph with the last one's content.
+
+var allPar = document.querySelectorAll('p');
+allPar.forEach(function(element) {
+    element.textContent = allPar[3].textContent;
+});
