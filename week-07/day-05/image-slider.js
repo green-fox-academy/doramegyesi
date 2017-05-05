23efc70d6f7d35e7d4c34976754f74efaefcf653
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-05 14:01:21 +0200

    refactored

--- week-07/day-05/image-slider.js
+++ week-07/day-05/image-slider.js
@@ -4,26 +4,27 @@ var allPictures = [
         title: 'words of wisdom',
         description: 'Never half-ass two things. Whole-ass one thing.'
     },
-    {source: 'images/2.jpg',
-    title: 'on women',
-    description: 'I am a simple man. I like pretty, dark-haired women and breakfast food.'
-},
-    {source: 'images/3.jpg',
-    title: 'on cats',
-    description: 'Any dog under fifty pounds is a cat, and cats are useless.'
-},
-    {source: 'images/4.jpg',
-    title: 'on great outdoors',
-    description: 'Fishing relaxes me. It is like yoga, except I still get to kill something.'
-},
-    {source: 'images/5.jpg',
-    title: 'on crying',
-    description: 'Keep your tears in your eyes belong.'
-},
-    {source: 'images/6.jpg',
-    title: 'on caring',
-    description: 'I am not interested in caring about people.'
-}];
+        {source: 'images/2.jpg',
+        title: 'on women',
+        description: 'I am a simple man. I like pretty, dark-haired women and breakfast food.'
+    },
+        {source: 'images/3.jpg',
+        title: 'on cats',
+        description: 'Any dog under fifty pounds is a cat, and cats are useless.'
+    },
+        {source: 'images/4.jpg',
+        title: 'on great outdoors',
+        description: 'Fishing relaxes me. It is like yoga, except I still get to kill something.'
+    },
+        {source: 'images/5.jpg',
+        title: 'on crying',
+        description: 'Keep your tears in your eyes belong.'
+    },
+        {source: 'images/6.jpg',
+        title: 'on caring',
+        description: 'I am not interested in caring about people.'
+    }
+];
 
 var mainPicture = document.querySelector('#main-image');
 var previous = document.querySelector('#previous');
