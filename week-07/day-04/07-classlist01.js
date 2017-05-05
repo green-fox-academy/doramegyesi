29bb8a6f65688cf1462a2e31b4de5ff88d1b6d29
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-05 06:44:09 +0200

    created the files

---
+++ week-07/day-04/07-classlist01.js
@@ -0,0 +1,6 @@
+/*  1. Does the third p have a cat class?
+    If so, alert 'YEAH CAT!'
+    2. If the fourth p has a 'dolphin' class, replace apple's content with 'pear'
+    3. If the first p has an 'apple' class, replace cat's content with 'dog'
+    4. Make apple red
+    5. Make balloon less balloon-like */
