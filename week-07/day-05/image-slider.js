b4c3f9e5c9aa9599265bad79f34ab76949437a5f
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-05 12:02:40 +0200

    images are in a list now

---
+++ week-07/day-05/image-slider.js
@@ -0,0 +1,25 @@
+var allPictures = [{
+    source: 'images/1.jpg',
+    title: 'being awesome',
+    description: 'Never half-ass two things. Whole-ass one thing.'
+},
+    {source: 'images/2.jpg',
+    title: 'on women',
+    description: 'I am a simple man. I like pretty, dark-haired women and breakfast food.'
+},
+    {source: 'images/3.jpg',
+    title: 'on cats',
+    description: 'Any dog under fifty pounds is a cat, and cats are useless.'
+},
+    {source: 'images/4.jpg',
+    title: 'on great outdoors',
+    description: 'Fishing relaxes me. It is like yoga, except I still get to kill something.'
+},
+    {source: 'images/5.jpg',
+    title: 'on crying',
+    description: 'Keep your tears in your eyes belong.'
+},
+    {source: 'images/6.jpg',
+    title: 'on caring',
+    description: 'I am not interested in caring about people.'
+}]
