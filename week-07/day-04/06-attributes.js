c811945204c16baefc04b01001e93354f7b4f6b4
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-05 06:09:45 +0200

    shows the attribute, yey

--- week-07/day-04/06-attributes.js
+++ week-07/day-04/06-attributes.js
@@ -1,5 +1,8 @@
-Write the image's url to the console.
-Replace the image with a picture of yourself.
-Make the link point to the Green Fox Academy website.
-Disable the second button.
-Replace its text with 'Don't click me!'.
+/*  1. Write the image's url to the console.
+    2. Replace the image with a picture of yourself.
+    3. Make the link point to the Green Fox Academy website.
+    4. Disable the second button.
+    5. Replace its text with 'Don't click me!'.*/
+
+var imageUrl = document.querySelector('img');
+console.log(imageUrl.getAttribute('src'));
