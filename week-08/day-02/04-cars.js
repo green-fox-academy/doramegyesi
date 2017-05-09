e4df60e985a06aba66dd3803efb06d3aa811247a
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-09 12:18:56 +0200

    04 done

---
+++ week-08/day-02/04-cars.js
@@ -0,0 +1,18 @@
+'use strict';
+// 1st
+// Call the ride method of the volvo, with 42 as a parameter
+
+var volvo = {
+  type: "Volvo",
+  fuel: 23,
+  consumption: 0.06,
+  kms: 43000,
+  ride: function (km) {
+    this.kms += km;
+    this.fuel -= km * this.consumption;
+    return this.fuel;
+}
+};
+
+
+console.log(volvo.ride(42));
