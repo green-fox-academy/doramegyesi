f77ffc29af91aa3412325c69bdcb8efe8501e74d
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-10 11:33:18 +0200

    does not work

---
+++ week-08/day-03/02-map.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var fruits = [
+  'melon',
+  'apple',
+  'strawberry',
+  'blueberry',
+  'pear',
+  'banana'
+];
+
+var lookForE = fruits.map(function(x) {
+    var es = []
+    if (x === 'e') {
+        es.push(x);
+    }
+    return es;
+})
+
+console.log(lookForE);
+
+// Create a new array of consists numbers that shows how many times the 'e' letter
+// occurs in the word stored under the same index at the fruits array!
+// Please use the map method.
