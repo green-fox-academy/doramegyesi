927700830e66e0c1d9203b1a2e792eb1171b4962
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-10 18:02:35 +0200

    some success!

---
+++ week-08/day-02/06-sharpie.js
@@ -0,0 +1,16 @@
+//Create a Sharpie constructor
+//We should know about each sharpie their color (which should be a string), width (which will be a floating point number), inkAmount (another floating point number)
+//When creating one, we need to specify the color and the width
+//Every sharpie is created with a default 100 as inkAmount
+//We can use() the sharpie objects
+//which decreases inkAmount by the width
+
+function sharpie(color, width, inkAmount) {
+    this.color = color,
+    this.width = width,
+    this.inkAmount = inkAmount
+    }
+
+var sharpieOne = new sharpie("black", 5.8, 2.3);
+
+console.log(sharpieOne.color);
