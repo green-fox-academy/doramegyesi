478614c54241c3adf12ce5a4c7cecdbfcfecfddf
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-11 16:50:49 +0200

    minor corrections

--- week-08/day-03/02-map.js
+++ week-08/day-03/02-map.js
@@ -22,9 +22,6 @@ var allTheEs = fruits.map(function(element) {
 
 console.log(allTheEs);
 
-
-
-
 // Create a new array of consists numbers that shows how many times the 'e' letter
 // occurs in the word stored under the same index at the fruits array!
 // Please use the map method.
