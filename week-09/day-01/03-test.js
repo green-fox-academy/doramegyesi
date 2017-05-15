30ec979b587feeb2932741f1163b60be9835c58a
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-15 15:10:05 +0200

    three tests passed

--- week-09/day-01/03-test.js
+++ week-09/day-01/03-test.js
@@ -2,8 +2,22 @@ var test = require('tape');
 var summarize = require('./03-sum.js');
 
 test('sum the elements', function(t) {
+  var actual = new summarize().sum([1, 2, 3]);
+  var expected = 6;
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('sum an empty array', function(t) {
   var actual = new summarize().sum([]);
   var expected = 0;
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('sum one element', function(t) {
+  var actual = new summarize().sum([5]);
+  var expected = 5;
   t.equal(actual, expected);
   t.end();
 });
