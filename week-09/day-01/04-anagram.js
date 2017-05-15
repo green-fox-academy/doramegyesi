981a01a52e6575cda9f4c463a12abac537463e07
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-15 16:39:07 +0200

    function works

---
+++ week-09/day-01/04-anagram.js
@@ -0,0 +1,11 @@
+/*Write a function, that takes two strings and returns a boolean value based on if the two strings are Anagramms or not.
+Create a test for that.*/
+
+function compare (a, b) {
+    var firstWord = a.split("").sort().join("");
+    var secondWord = b.split("").sort().join("");
+    return firstWord === secondWord;
+};
+
+console.log(compare('dog', 'god'));
+console.log(compare('dog', 'cat'));
