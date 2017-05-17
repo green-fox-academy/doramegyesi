b0fd62750fccab11b02b68d75d8da590e56be084
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-17 21:01:24 +0200

    init files

---
+++ week-08/day-05/reddit.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var domain = 'https://time-radish.glitch.me';
+var body = document.querySelector('body');
+var container = document.querySelector('.container');
+var postForm = document.querySelector('.postform');
+var newPostButton = document.querySelector('.newpost');
+
+/* plans
+article container html-be (innerhtml-t torolni)
+PUT utan gET is kell megint
+NEW POST button
+form letrehozasnal article container innerhtml torles
+new post button style visibility = hidden, JS-ben
+post to servernel (input mezonel) nem innerhtml hanem value kell
+new post button visibility vissza!*/
