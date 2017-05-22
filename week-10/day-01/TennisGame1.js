65b0ab3b98b99bf71fcf4da5fdab59a13739e775
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-22 10:08:28 +0200

    added brackets

--- week-10/day-01/TennisGame1.js
+++ week-10/day-01/TennisGame1.js
@@ -6,10 +6,11 @@ var TennisGame1 = function(player1Name, player2Name) {
 };
 
 TennisGame1.prototype.wonPoint = function(playerName) {
-    if (playerName === "player1")
-        this.m_score1 += 1;
-    else
-        this.m_score2 += 1;
+    if (playerName === "player1") {
+        this.m_score1++;
+    } else {
+        this.m_score2++;
+    }
 };
 
 TennisGame1.prototype.getScore = function() {
@@ -32,14 +33,20 @@ TennisGame1.prototype.getScore = function() {
         }
     } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
         var minusResult = this.m_score1 - this.m_score2;
-        if (minusResult === 1) score = "Advantage player1";
-        else if (minusResult === -1) score = "Advantage player2";
-        else if (minusResult >= 2) score = "Win for player1";
-        else score = "Win for player2";
+        if (minusResult === 1) {
+            score = "Advantage player1";
+        } else if (minusResult === -1) {
+            score = "Advantage player2";
+        } else if (minusResult >= 2) {
+            score = "Win for player1";
+        } else {
+            score = "Win for player2";
+        }
     } else {
         for (var i = 1; i < 3; i++) {
-            if (i === 1) tempScore = this.m_score1;
-            else {
+            if (i === 1) {
+                tempScore = this.m_score1;
+            } else {
                 score += "-";
                 tempScore = this.m_score2;
             }
