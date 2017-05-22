87642973be04f3416c16ba63a6215e6f069a0667
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-22 10:53:25 +0200

    added brackets

--- week-10/day-01/TennisGame3.js
+++ week-10/day-01/TennisGame3.js
@@ -15,19 +15,19 @@ TennisGame3.prototype.getScore = function() {
     } else {
         if (this.p1 == this.p2)
             return "Deuce";
-        s = this.p1 > this.p2 ? this.p1N : this.p2N;
+        s = (this.p1 > this.p2) ? this.p1N : this.p2N;
         return ((this.p1 - this.p2) * (this.p1 - this.p2) == 1) ? "Advantage " + s : "Win for " + s;
     }
 };
 
 TennisGame3.prototype.wonPoint = function(playerName) {
-    if (playerName == "player1")
+    if (playerName == "player1") {
         this.p1 += 1;
-    else
+    } else {
         this.p2 += 1;
-
+    }
 };
 
 if (typeof window === "undefined") {
     module.exports = TennisGame3;
-}
+};
