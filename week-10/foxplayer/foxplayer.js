9751848716bcd85aabd96f652d2d43d0b1ad5009
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-24 19:33:53 +0200

    server works

--- week-10/foxplayer/foxplayer.js
+++ week-10/foxplayer/foxplayer.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const cors = require('cors');
 const express = require('express');
 const app = express();
+
+app.use(cors());
 
 var allPlaylists = [
 	{ "id": 1, "title": "Favorites", "system": 1},
@@ -10,12 +13,21 @@ var allPlaylists = [
 	{ "id": 5, "title": "Fox house", "system": 0},
 ];
 
+var allTracks = [
+	{ "id": 21, "title": "Halahula", "artist": "Untitled artist", "duration": 545, "path": "c:/music/halahula.mp3" },
+	{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3" }
+];
+
 app.get('/', function(req, res){
     res.send('FOXPLAYER');
 });
 
 app.get('/playlists', function(req, res){
     res.send(allPlaylists);
+});
+
+app.get('/playlist-tracks:playlist_id', function(req, res){
+    res.send(allTracks);
 });
 
 app.listen(3000, function(){
