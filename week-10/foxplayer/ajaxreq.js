a05cccb9a5a4a131fb23771b91d71c9d8fb5c3dc
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-25 11:00:35 +0200

    gets the tracklist w/ title but does overflow even work in flexbox?!

--- week-10/foxplayer/ajaxreq.js
+++ week-10/foxplayer/ajaxreq.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const playlist = document.querySelector('.playlist')
+const tracks = document.querySelector('.tracks')
 
 const getPlaylists = function() {
     const req = new XMLHttpRequest();
@@ -22,4 +23,45 @@ const getPlaylists = function() {
     }
 };
 
+const getTracklist = function() {
+    const req = new XMLHttpRequest();
+    const method = 'GET';
+    const url = 'http://localhost:3000/playlist-tracks:playlist_id';
+    req.open(method, url);
+    req.send();
+    req.onreadystatechange = function() {
+        if (req.readyState === 4 && req.status === 200) {
+            var tracklist = JSON.parse(req.response);
+            console.log(tracklist);
+            tracklist.forEach(function(item) {
+                var tracklistItem = document.createElement('div');
+                tracklistItem.setAttribute('class', 'tracklistItem')
+                tracklistItem.innerText = item.title;
+                tracks.appendChild(tracklistItem);
+            });
+        }
+    }
+};
+
+/*const audio = document.querySelector('audio')
+
+function pausePlay(event) {
+    switch (event.keyCode) {
+        case 32:
+            if (play) {
+                audio.pause();
+                play = false;
+            } else {
+                audio.play();
+                play = true;
+            }
+            break;
+        }
+  return false;
+}
+
+window.addEventListener("keydown", onKeyDown, false);
+</script>
+*/
 getPlaylists();
+getTracklist();
