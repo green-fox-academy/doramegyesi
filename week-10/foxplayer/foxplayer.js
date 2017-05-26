23ad8e511765fbf54259ed83ed14d499c0cad3ed
Megyesi Dora <dora.megyesi@gmail.com>
2017-05-26 13:34:45 +0200

    database is created, connection woooorks!

--- week-10/foxplayer/foxplayer.js
+++ week-10/foxplayer/foxplayer.js
@@ -2,38 +2,44 @@
 
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mysql = require("mysql");
 const express = require('express');
 const app = express();
+var response;
 
 app.use('/assets', express.static('assets'));
 app.use('/img', express.static('img'));
 app.use(bodyParser.json());
 app.use(cors());
 
-var allPlaylists = [
-	{ "id": 1, "title": "Songs for running", "system": 1},
-	{ "id": 2, "title": "Music for programming", "system": 0},
-	{ "id": 3, "title": "Relaxing", "system": 0},
-	{ "id": 4, "title": "Crazy party music", "system": 0},
-	{ "id": 5, "title": "Best roadtrip songs", "system": 0},
-];
+const connect = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "foxplayer"
+});
 
-var allTracks = [
-	{ "id": 21, "title": "Halahula", "artist": "Untitled artist", "duration": 545, "path": "c:/music/halahula.mp3" },
-	{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3" },
-	{ "id": 28, "title": "Good song", "artist": "Cool guy", "duration": 921, "path": "c:/music/something.mp3" },
-	{ "id": 72, "title": "Makes you cry", "artist": "Sassy sisters", "duration": 755, "path": "c:/music/bla.mp3" },
-	{ "id": 53, "title": "Party hymn", "artist": "Crazy Dudes", "duration": 155, "path": "c:/music/zzla.mp3" },
-	{ "id": 62, "title": "Your moms fav song", "artist": "Man With Sad Eyes", "duration": 84, "path": "c:/music/mom.mp3" },
-	{ "id": 11, "title": "Relax", "artist": "A Girl", "duration": 145, "path": "c:/music/girl.mp3" }
-];
+connect.connect(function(err){
+  if(err){
+    console.log("could not connect");
+  }
+  console.log("connected successfully");
+});
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/foxplayer.html');
 });
 
 app.get('/playlists', function(req, res){
-    res.send(allPlaylists);
+	connect.query('SELECT * FROM playlists', function(err, rows) {
+		if (err) {
+			console.log('could not reach the playlists', err.message)
+		} else {
+			response = rows;
+			console.log(response);
+		}
+        res.send(response);
+	});
 });
 
 app.get('/playlist-tracks:playlist_id', function(req, res){
