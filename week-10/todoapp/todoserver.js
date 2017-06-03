9951ea6568f14d488cfb89335a0d15eef081d766
Megyesi Dora <dora.megyesi@gmail.com>
2017-06-03 15:59:25 +0200

    necessary packages are installed

---
+++ week-10/todoapp/todoserver.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const mysql = require("mysql");
+const cors = require('cors');
+const bodyParser = require('body-parser');
