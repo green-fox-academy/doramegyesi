a3db18f50fd3c601f39f7278aecd9bde9271bfc2
Megyesi Dora <dora.megyesi@gmail.com>
2017-06-03 16:03:11 +0200

    server runs

--- week-10/todoapp/todoserver.js
+++ week-10/todoapp/todoserver.js
@@ -5,3 +5,16 @@ const app = express();
 const mysql = require("mysql");
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
+app.use('/assets', express.static('assets'));
+app.use('/img', express.static('img'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/todoapp.html');
+});
+
+app.listen(3000, function() {
+    console.log('server is running smoothly');
+});
