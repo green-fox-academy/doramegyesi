7ae98e018bf13cb241cc73912955cf08c91ea84f
Dora Megyesi <dora.megyesi@gmail.com>
2017-07-10 09:40:30 +0200

    makes two arrays of the two words

--- final-exam/anagram_checker.js
+++ final-exam/anagram_checker.js
@@ -4,7 +4,18 @@ let anagramCheker = function(word1, word2) {
     if (word1.length != word2.length) {
         console.log("Those are not anagrams, dude")
     }
+    else {
+        let first = [];
+        let second = [];
+        for (let i = 0; i < word1.length; i++) {
+            first.push(word1[i]);
+        };
+        console.log(first);
+        for (let i = 0; i < word2.length; i++) {
+            second.push(word2[i]);
+        };
+        console.log(second);
+    };
 }
 
-
-anagramCheker("cockroach", "sloth");
+anagramCheker("sloth", "sloth");
