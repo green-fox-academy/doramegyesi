fb37ffe205c3290f93a28f71bcb9582ffe4b7ebd
Dora Megyesi <dora.megyesi@gmail.com>
2017-07-10 11:54:15 +0200

    finally works properly

--- final-exam/anagram_checker.js
+++ final-exam/anagram_checker.js
@@ -1,40 +1,15 @@
 // Create a function that takes two strings and decides if one is anagram of the other
 
-let anagramCheker = function(word1, word2) {
-    if (word1.length != word2.length) {
-        console.log("Those are not anagrams, dude")
-        return false;
-    }
-    else {
-        let first = [];
-        let second = [];
-        let counter = 0;
-        for (let i = 0; i < word1.length; i++) {
-            first.push(word1[i]);
-        };
-        console.log(first);
-        for (let i = 0; i < word2.length; i++) {
-            second.push(word2[i]);
-        };
-        console.log(second);
-        for (let i = 0; i < first.length; i++) {
-            while (i < second.length) {
-                if (i === second[i]) {
-                    counter++;
-                    break;
-                }
-                i++;
+let anagramChecker = function(str1, str2) {
+    if (str1.length === str2.length) {
+        for (let i = 0; i <str1.length; i++) {
+            if (str1[i] !== str2[-1]) {
+                return true;
             }
-        };
-        console.log(counter)
-        if (word1.length === counter) {
-            return true;
         }
-        else {
-            return false;
-        };
-    };
-};
-console.log(anagramCheker("sloth", "sloth"));
-console.log(anagramCheker("dog", "god"));
-console.log(anagramCheker("cockroach", "sloth"));
+    } return false;
+}
+
+console.log(anagramChecker("god", "dogo"));
+console.log(anagramChecker("god", "dog"));
+console.log(anagramChecker("ogd", "dog"));
