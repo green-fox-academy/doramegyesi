4ae8954bb70bc0a467f6e9d7640a3eaba855b047
Dora Megyesi <dora.megyesi@gmail.com>
2017-07-10 09:27:44 +0200

    checked the length of words

--- final-exam/anagram_checker.js
+++ final-exam/anagram_checker.js
@@ -1,1 +1,10 @@
-Create a function that takes two strings and decides if one is anagram of the other
+// Create a function that takes two strings and decides if one is anagram of the other
+
+let anagramCheker = function(word1, word2) {
+    if (word1.length != word2.length) {
+        console.log("Those are not anagrams, dude")
+    }
+}
+
+
+anagramCheker("cockroach", "sloth");
