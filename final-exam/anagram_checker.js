e49105b48be4c04598a330c07134ea299b353db4
Dora Megyesi <dora.megyesi@gmail.com>
2017-07-10 09:59:32 +0200

    finally checks the words for anagrams

--- final-exam/anagram_checker.js
+++ final-exam/anagram_checker.js
@@ -3,10 +3,12 @@
 let anagramCheker = function(word1, word2) {
     if (word1.length != word2.length) {
         console.log("Those are not anagrams, dude")
+        return false;
     }
     else {
         let first = [];
         let second = [];
+        let counter = 0;
         for (let i = 0; i < word1.length; i++) {
             first.push(word1[i]);
         };
@@ -15,7 +17,24 @@ let anagramCheker = function(word1, word2) {
             second.push(word2[i]);
         };
         console.log(second);
+        for (let i = 0; i < first.length; i++) {
+            while (i < second.length) {
+                if (i === second[i]) {
+                    counter++;
+                    break;
+                }
+                i++;
+            }
+        };
+        console.log(counter)
+        if (word1.length === counter) {
+            return true;
+        }
+        else {
+            return false;
+        };
     };
-}
-
-anagramCheker("sloth", "sloth");
+};
+console.log(anagramCheker("sloth", "sloth"));
+console.log(anagramCheker("dog", "god"));
+console.log(anagramCheker("cockroach", "sloth"));
